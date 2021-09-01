5f9e0d859112803a39b89bb649100d864b947353
ashok49473 <ashok49473@gmail.com>
2021-09-01 15:52:46 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let str = "Welcome to the javaTpoint.com";    
+document.write(" <b> The given string is: </b>", str);  
+document.write("<br>");  
+let res = str.includes('tO');  
+document.write(" <b> The result is: </b> ", res);   
